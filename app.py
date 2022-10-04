e147173158b2a3f2e2f8c3b3242509886b395dcf
syedsajjadaskari <syedsajjad62@gmail.com>
2022-10-04 12:16:47 +0530

    add exception and loggger

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
-
+from Housing.logger import logging
+from Housing.exception import HousingException
 app=Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        raise HousingException(e, sys) from e        logging.info(housing.error_message)
+        logging.info("we are testing logging")
     return "CI CD pipeline has been established."
 
 
